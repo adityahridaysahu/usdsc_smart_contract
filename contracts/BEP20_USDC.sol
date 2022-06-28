82bd3efa8a4c16552eadfd6d08f0a57d97ddc88d
Aditya Hriday Sahu <75262419+adityahridaysahu@users.noreply.github.com>
2022-06-28 17:48:58 +0530

    Create BEP20_USDC.sol

---
+++ contracts/BEP20_USDC.sol
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "https://github.com/pancakeswap/pancake-swap-lib/blob/master/contracts/token/BEP20/IBEP20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/GSN/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/adityahridaysahu/usdsc_smart_contract/blob/main/contracts/Address.sol";
+
+
+
+contract BEP20 is Context, IBEP20, Ownable {
+    using SafeMath for uint256;
+    using Address for address;
+
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor(string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = 18;
+    }
+
+
+    function getOwner() external override view returns (address) {
+        return owner();
+    }
+
+    function name() public override view returns (string memory) {
+        return _name;
+    }
+
+    function decimals() public override view returns (uint8) {
+        return _decimals;
+    }
+
+ 
+    function symbol() public override view returns (string memory) {
+        return _symbol;
+    }
+
+
+    function totalSupply() public override view returns (uint256) {
+        return _totalSupply;
+    }
+
+
+    function balanceOf(address account) public override view returns (uint256) {
+        return _balances[account];
+    }
+
+ 
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+
+    function allowance(address owner, address spender) public override view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(
+            sender,
+            _msgSender(),
+            _allowances[sender][_msgSender()].sub(amount, 'BEP20: transfer amount exceeds allowance')
+        );
+        return true;
+    }
+
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender].sub(subtractedValue, 'BEP20: decreased allowance below zero')
+        );
+        return true;
+    }
+
+ 
+    function mint(uint256 amount) public onlyOwner returns (bool) {
+        _mint(_msgSender(), amount);
+        return true;
+    }
+
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal {
+        require(sender != address(0), 'BEP20: transfer from the zero address');
+        require(recipient != address(0), 'BEP20: transfer to the zero address');
+
+        _balances[sender] = _balances[sender].sub(amount, 'BEP20: transfer amount exceeds balance');
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), 'BEP20: mint to the zero address');
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), 'BEP20: burn from the zero address');
+
+        _balances[account] = _balances[account].sub(amount, 'BEP20: burn amount exceeds balance');
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        require(owner != address(0), 'BEP20: approve from the zero address');
+        require(spender != address(0), 'BEP20: approve to the zero address');
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _setupDecimals(uint8 decimals_) internal {
+        _decimals = decimals_;
+    }
+
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(
+            account,
+            _msgSender(),
+            _allowances[account][_msgSender()].sub(amount, 'BEP20: burn amount exceeds allowance')
+        );
+    }
+}
+
+
+contract ServiceReceiver is Ownable {
+
+    mapping (bytes32 => uint256) private _prices;
+
+    event Created(string serviceName, address indexed serviceAddress);
+
+    function pay(string memory serviceName) public payable {
+        require(msg.value == _prices[_toBytes32(serviceName)], "ServiceReceiver: incorrect price");
+
+        emit Created(serviceName, _msgSender());
+    }
+
+    function getPrice(string memory serviceName) public view returns (uint256) {
+        return _prices[_toBytes32(serviceName)];
+    }
+
+    function setPrice(string memory serviceName, uint256 amount) public onlyOwner {
+        _prices[_toBytes32(serviceName)] = amount;
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        payable(owner()).transfer(amount);
+    }
+
+    function _toBytes32(string memory serviceName) private pure returns (bytes32) {
+        return keccak256(abi.encode(serviceName));
+    }
+}
+
+
+abstract contract ServicePayer {
+
+    constructor (address payable receiver, string memory serviceName) payable {
+        ServiceReceiver(receiver).pay{value: msg.value}(serviceName);
+    }
+}
+
+
+contract BEP20_USDSC is BEP20, ServicePayer {
+
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 initialBalance,
+        address payable feeReceiver
+    )
+        BEP20(name, symbol)
+        ServicePayer(feeReceiver, "StandardBEP20")
+        payable
+    {
+        require(initialBalance > 0, "StandardBEP20: supply cannot be zero");
+
+        _setupDecimals(decimals);
+        _mint(_msgSender(), initialBalance);
+    }
+}
